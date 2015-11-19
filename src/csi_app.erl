ceb121694292dcb2e3a17fbce6dcb74824538e83
sv <sv@cloudgears.com>
2015-11-19 23:07:12 +0100

    start_link

--- src/csi_app.erl
+++ src/csi_app.erl
@@ -1,15 +1,19 @@
 -module(csi_app).
 -behaviour(application).
 
--export([start/2, stop/1]).
+-export([start/2, start_link/2, stop/1]).
 
-start(_StartType, _StartArgs) ->
+
+start(_StartType, _StartParams) ->
+  start_link(8080, "/ws").
+
+start_link(Port, Path) ->
   Dispatch = cowboy_router:compile([
-		{'_', [
-			{"/ws", csi_handler, []}
-		]}
-	]),
-	cowboy:start_http(http, 100, [{port, 8080}], [{env, [{dispatch, Dispatch}]}]).
+    {'_', [
+      {Path, csi_handler, []}
+    ]}
+  ]),
+  cowboy:start_http(http, 100, [{port, Port}], [{env, [{dispatch, Dispatch}]}]).
 
 stop(_State) ->
   ok.   
